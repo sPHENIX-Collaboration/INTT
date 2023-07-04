8d1f9af577f1141bc9cc483feaeaac3e8d9e53e4
josephbertaux <jbertau@purdue.edu>
2023-07-04 18:45:30 -0400

    Some tinkering with the Makefile

--- general_codes/josephb/codes/intt_alignment/src/test.cc
+++ general_codes/josephb/codes/intt_alignment/src/test.cc
@@ -1,26 +1,31 @@
-#include "foo/AlignTransform.h"
+//#include "foo/AlignTransform.h"
+
+#include <cstdio>
 
 int main()
 {
-	AlignTransform a;
-	a.Ang("z") = atan(1.0);
-	a.SetTransformFromAngles();
-	a.Print();
-	std::cout << std::endl;
 
-	AlignTransform b;
-	b.Ang("z") = 2.0 / 3.0 * atan(1.0);
-	b.SetTransformFromAngles();
-	b.Print();
-	std::cout << std::endl;
+	printf("Hello World\n");
 
-	a = a * b;
-	a.SetAnglesFromTransform();
-	a.Print();
-	std::cout << std::endl;
+	//AlignTransform a;
+	//a.Ang("z") = atan(1.0);
+	//a.SetTransformFromAngles();
+	//a.Print();
+	//std::cout << std::endl;
 
-	std::cout << a.Ang(0) << std::endl;
-	std::cout << 5.0 / 3.0 * atan(1.0) << std::endl;
+	//AlignTransform b;
+	//b.Ang("z") = 2.0 / 3.0 * atan(1.0);
+	//b.SetTransformFromAngles();
+	//b.Print();
+	//std::cout << std::endl;
+
+	//a = a * b;
+	//a.SetAnglesFromTransform();
+	//a.Print();
+	//std::cout << std::endl;
+
+	//std::cout << a.Ang(0) << std::endl;
+	//std::cout << 5.0 / 3.0 * atan(1.0) << std::endl;
 
 	return 0;
 }
