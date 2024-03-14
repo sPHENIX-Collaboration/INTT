e98d74f0e5014f51fec860e70bca594b9a08312c
gwd213 <jaeinhwang213@gmail.com>
2024-03-14 09:56:18 -0400

    InttBCOMap.cc/h added

---
+++ general_codes/Jaein/Calibration/InttBCOMap.h
@@ -0,0 +1,37 @@
+#ifndef INTTBCOMAP_H
+#define INTTBCOMAP_H
+
+#include <array>
+#include <string>
+#include "InttMapping.h"
+
+class CDBTTree;
+
+class InttBCOMap
+{
+public:
+  InttBCOMap() = default;
+  virtual ~InttBCOMap() {}
+
+  virtual int LoadFromCDB(std::string const &calibname); 
+//  /sphenix/tg/tg01/commissioning/INTT/QA/bco_bcofull_difference/CDB/2023/cdb_bco_20869.root
+  virtual int LoadFromFile(std::string const &filename); 
+
+  virtual bool IsBad(int const &felix_server,
+                     int const &fexlix_channel,
+                     Long64_t const &bco_full,
+                     const int &bco); 
+
+  virtual bool IsBad(InttNameSpace::RawData_s const &rawdata, Long64_t const &bco_full, const int &bco);
+  virtual bool IsBad(InttNameSpace::Offline_s const &offline, Long64_t const &bco_full, const int &bco);
+
+//For debugging
+//  bool isBCOPeakTest(int felix_server_, int felix_channel_, int testvalue); 
+
+protected:
+  int LoadFromCDBTTree(CDBTTree &cdbttree); 
+private:
+  typedef std::array<std::array<int, 14>, 8> BCOArray;
+  BCOArray m_bco; //[Felix server][Felix channel]
+};
+#endif
