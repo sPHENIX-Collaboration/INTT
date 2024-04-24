d4aaad83cecb11d1dab70d8c9222334dd0124b37
Genki Nukazuka <genki.nukazuka@riken.jp>
2024-04-23 21:48:12 -0400

    Updates on constant_values.hh in Jaein's cosmic macro

--- general_codes/Jaein/Cosmics/macro/constant_values.hh
+++ general_codes/Jaein/Cosmics/macro/constant_values.hh
@@ -4,6 +4,7 @@
 // Important variables ///////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////
 const unsigned int kFelix_num = 8;
+const unsigned int kLadders_in_felix_num = 14;
 const unsigned int kLadders_in_roc_num = 7;
 const unsigned int kChip_num = 26;
 const unsigned int kCh_num = 128;
@@ -22,7 +23,13 @@ const string kIntt_dst_dir	= kIntt_data_dir + "dst_files/" + to_string( kYear ) 
 const string kIntt_root_dir	= kIntt_data_dir + "root_files/" + to_string( kYear ) + "/";
 const string kIntt_cdb_dir	= kIntt_data_dir + "CDB_files/" + to_string( kYear ) + "/";
 
+/*
 const string kIntt_qa_dir	= "/sphenix/tg/tg01/commissioning/INTT/QA/";
-const string kIntt_hitmap_dir	= kIntt_qa_dir + "/hitmap/" + to_string( kYear ) + "/";
-const string kIntt_qa_cosmics_dir = kIntt_qa_dir + "/cosmics/" + to_string( kYear ) + "/";
-
+const string kIntt_hitmap_dir	= kIntt_qa_dir + "hitmap/" + to_string( kYear ) + "/";
+const string kIntt_hotmap_dir	= kIntt_qa_dir + "hotdeadmap/" + to_string( kYear ) + "/";
+const string kIntt_qa_cosmics_dir = kIntt_qa_dir + "cosmics/" + to_string( kYear ) + "/";
+*/
+const string kIntt_qa_dir	= "/sphenix/tg/tg01/commissioning/INTT/QA/";
+const string kIntt_hitmap_dir	= kIntt_qa_dir + "hitmap/" + to_string( kYear ) + "/";
+const string kIntt_hotmap_dir	= kIntt_qa_dir + "hotdeadmap/";
+const string kIntt_qa_cosmics_dir = kIntt_qa_dir + "cosmics/";
