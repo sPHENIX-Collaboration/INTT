42d3dc27b10c61e4a9a017c0eb070b559b524240
Genki Nukazuka <genki.nukazuka@riken.jp>
2025-01-31 12:15:32 -0500

    Some modification for Run2025

--- general_codes/hachiya/InttEventTree/ConvertInttData.h
+++ general_codes/hachiya/InttEventTree/ConvertInttData.h
@@ -7,6 +7,7 @@
 int Init (const char* outRootfile); //++CINT 
 //int Process_event (Event *e); //++CINT 
 int Run (const char* evtfile); //++CINT 
-int module_map(std::string="/home/phnxrc/INTT/map_ladder/");
+//int module_map(std::string="/home/phnxrc/INTT/map_ladder/");
+int module_map(std::string="/sphenix/tg/tg01/commissioning/INTT/map_ladder/" );
 
 #endif /* __CONVERTINTTDATA_H__ */
