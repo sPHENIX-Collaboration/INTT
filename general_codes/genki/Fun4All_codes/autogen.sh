e7cd1ac9ce6168965849cbe6c6fdb5ee676bd7c9
hahahachiya <hachiya@rcf.rhic.bnl.gov>
2025-12-14 12:01:24 -0500

    link files are changed to actual files and all unnecessary files are removed

--- general_codes/genki/Fun4All_codes/autogen.sh
+++ general_codes/genki/Fun4All_codes/autogen.sh
@@ -1,1 +1,8 @@
-temp/autogen.sh
+#!/bin/sh
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+(cd $srcdir; aclocal -I ${OFFLINE_MAIN}/share;\
+libtoolize --force; automake -a --add-missing; autoconf)
+
+$srcdir/configure  "$@"
